d61fd49eb988c51fe2991b52eadc15c411cc18f0
Ziad Ismael <ziad.ismael@icloud.com>
2025-09-20 05:09:09 +0300

    fix: fixed the imports of Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
     return (
         <main>
             <div>
-                <h1>Welcome to my 3D Portfolio!</h1>
+                <h1 className="text-3xl">Welcome to my 3D Portfolio!</h1>
             </div>
         </main>
     )
