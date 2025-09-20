4a44912ea23cd52c198ab58a2fb00b290f25c3eb
Ziad Ismael <ziad.ismael@icloud.com>
2025-09-20 04:44:52 +0300

    Setting up the project with Vite and React

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1>Welcome to my 3D Portfolio!</h1>
+            </div>
+        </main>
+    )
+}
+export default App
